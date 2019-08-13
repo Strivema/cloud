c420c75f9b00bd8965b814ddc7af270bdf557b95
ray.ma <ray.ma@chinaentropy.com>
2019-08-13 18:45:28 +0800

    Initial commit

---
+++ cloud-moniter/src/main/java/com/ray/CloudMoniterApplication.java
@@ -0,0 +1,13 @@
+package com.ray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudMoniterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudMoniterApplication.class, args);
+    }
+
+}
---
+++ cloud-zipkin/src/main/java/com/ray/CloudZipkinApplication.java
@@ -0,0 +1,13 @@
+package com.ray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudZipkinApplication.class, args);
+    }
+
+}
